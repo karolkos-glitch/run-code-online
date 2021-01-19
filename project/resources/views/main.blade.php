591111f9765643496d8d86d27941f2e8e6f45092
Karol <karol99kos@gmail.com>
2021-01-19 13:20:16 +0100

    beta layout created

--- project/resources/views/main.blade.php
+++ project/resources/views/main.blade.php
@@ -19,21 +19,23 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="mx-auto">
+        <div class="mx-auto max-w-screen-lg">
             <main class="h-screen">
-                <header>
+                <header class="h-1/6">
                     <h1 class="text-center text-4xl font-semibold text-blue-700">netcode</h1>
                     <p class="text-center text-base text-blue-400">Try it online</p>
                 </header>
-                <section>
-                    <div id="code">
-                        <form method="post">
-                            <textarea>
+                <section class="w-full h-5/6 border-solid border-4 border-yellow-500">
+                    <div id="code" class="h-2/3 border-solid border-4 border-blue-700">
+                        <form method="post" class="flex justify-center flex-wrap h-full">
+                            <textarea class="h-5/6 w-full">
+
                             </textarea>
+                            <button class="border-solid border-4 border-blue-700 px-2">run code</button>
                         </form>
                     </div>
-                    <div id="result">
-                     
+                    <div id="result" class="h-1/3 border-solid border-4 border-red-700">
+         
                     </div>
                 </section>
             </main>
